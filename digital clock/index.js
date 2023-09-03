73e51cb415d6e49a0ef89836d8e7a944031eeb18
Parishri07 <136893851+Parishri07@users.noreply.github.com>
2023-09-03 16:45:49 +0530

    Delete digital clock directory

--- digital clock/index.js
+++
@@ -1,9 +0,0 @@
-function change(){
-    var hours = new Date().getHours();
-    var minutes = new Date().getMinutes();
-    var seconds = new Date().getSeconds();
-    
-    var time = hours +":"+ minutes +":"+ seconds;
-    document.getElementsByClassName("container")[0].innerHTML = time;
-}
-setInterval(change, 1000);
